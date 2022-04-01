63dbcba3fe01bc6ea261ebefd4874fa6f1b0b1ef
Rafael <raphaelsantosajs@gmail.com>
2022-03-31 22:46:34 -0300

    v3

--- js/index.js
+++ js/index.js
@@ -18,7 +18,7 @@ const secretWords = [
     ["Hamster", "Animal"],
 ]
 
-const errors = 0 // Armazena erros para atualizar a imagem da forca.
+let errors = 0 // Armazena erros para atualizar a imagem da forca.
 
 // Gera número aleatório.
 const getRnd = (min, max) => {
@@ -30,22 +30,39 @@ const getRnd = (min, max) => {
 // Definindo valores iniciais.
 const rnd        = getRnd(0, secretWords.length),
       sortedTip  = secretWords[rnd][1],
-      sortedWord = secretWords[rnd][0]
+      sortedWord = (secretWords[rnd][0]).toUpperCase()
 
 tip.innerHTML += sortedTip
 keyword.innerHTML = "⎽".repeat(sortedWord.length)
-
-
-// Varrendo palavra secreta e setando letras encontradas
-const showWord = () => {
-    // sortedWord.length
-}
+let cache = keyword.textContent
 
 // Atribuindo evento em cada botão dentro da variavel 'buttons'
 buttons.forEach(button => button.addEventListener("click", () => {
-    // for (let cont = 0; cont < secretWords.length; cont++) {
-    //     if (secretWords[cont][0].includes(button.textContent)) {
+    // Quando clicado, o botão é desativado.
+    button.className = "btKeyDesabled"
 
-    //     }
-    // }
+    // Botao que tenha a mesma letra dentro da palavra sorteada, é destacado.
+    // Revela a letra encontrada nas posições, dentro da frase sorteada. 
+    if (sortedWord.includes(button.textContent)) {
+        button.style.backgroundColor = "rgb(0 255 0 / 15%)"
+        
+        // Aplicando as letras encontradas nas posições da palavra sorteada que esta oculta.
+        for(let count = 0; count < sortedWord.length; count++){
+            if(sortedWord[count] == button.textContent) {
+                console.log(cache[count])
+            }
+        }
+    }
+    else {
+        // Caso a letra do botao clicado não estiver na palavra sorteada,
+        // então o 'errors' é incrementado e a imagem da forca é definida com a variável.
+        errors++
+        image.style.backgroundImage = `url(./images/forca${errors}.png)`
+
+        // Caso atinga 6 erros, então o jogo acaba.
+        if(errors == 6) {
+            alert("você perdeu!")
+            window.location.reload(true)
+        }
+    }
 }))
